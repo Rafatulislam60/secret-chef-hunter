b40dd4eeec6f5a28a7fb815ccb0fa7107edf9513
Rafatulislam60 <rafatulislam67@gmail.com>
2023-05-02 14:18:22 +0600

    footer added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
 
+
 const router = createBrowserRouter([
     {
         path: '/',
